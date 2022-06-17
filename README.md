5f6eedcb86e27e22e0d3a19b5e15c2ced6e9e85f
qalle2 <qalle85@gmail.com>
2022-06-17 04:20:11 +0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nes-pride
-under construction
+Convert images of pride flags into [NES](https://en.wikipedia.org/wiki/Nintendo_Entertainment_System)-compatible format.
+
+The Python script that converts the image data is under construction; the NES program hasn't even been started yet.
+
+[Source of flags](https://en.wikipedia.org/wiki/Pride_flag#Gallery). The license of this project does not apply to the flag images (I guess).
---
+++ convert.py
@@ -0,0 +1,193 @@
+# convert images of pride flags into NES-compatible format
+# under construction
+
+import itertools, os, sys
+from PIL import Image  # Pillow, https://python-pillow.org
+
+IMAGE_DIR = "img"
+IMAGE_EXT = ".png"
+NES_BG_COLOR = 0x0f  # NES background color (black)
+
+# NES master palette
+# key=index, value=(red, green, blue); source: FCEUX (fceux.pal)
+# colors omitted (hexadecimal): 0d-0e, 1d-20, 2e-2f, 3e-3f
+NES_PALETTE = {
+    0x00: (0x74, 0x74, 0x74),
+    0x01: (0x24, 0x18, 0x8c),
+    0x02: (0x00, 0x00, 0xa8),
+    0x03: (0x44, 0x00, 0x9c),
+    0x04: (0x8c, 0x00, 0x74),
+    0x05: (0xa8, 0x00, 0x10),
+    0x06: (0xa4, 0x00, 0x00),
+    0x07: (0x7c, 0x08, 0x00),
+    0x08: (0x40, 0x2c, 0x00),
+    0x09: (0x00, 0x44, 0x00),
+    0x0a: (0x00, 0x50, 0x00),
+    0x0b: (0x00, 0x3c, 0x14),
+    0x0c: (0x18, 0x3c, 0x5c),
+    0x0f: (0x00, 0x00, 0x00),
+    0x10: (0xbc, 0xbc, 0xbc),
+    0x11: (0x00, 0x70, 0xec),
+    0x12: (0x20, 0x38, 0xec),
+    0x13: (0x80, 0x00, 0xf0),
+    0x14: (0xbc, 0x00, 0xbc),
+    0x15: (0xe4, 0x00, 0x58),
+    0x16: (0xd8, 0x28, 0x00),
+    0x17: (0xc8, 0x4c, 0x0c),
+    0x18: (0x88, 0x70, 0x00),
+    0x19: (0x00, 0x94, 0x00),
+    0x1a: (0x00, 0xa8, 0x00),
+    0x1b: (0x00, 0x90, 0x38),
+    0x1c: (0x00, 0x80, 0x88),
+    0x21: (0x3c, 0xbc, 0xfc),
+    0x22: (0x5c, 0x94, 0xfc),
+    0x23: (0xcc, 0x88, 0xfc),
+    0x24: (0xf4, 0x78, 0xfc),
+    0x25: (0xfc, 0x74, 0xb4),
+    0x26: (0xfc, 0x74, 0x60),
+    0x27: (0xfc, 0x98, 0x38),
+    0x28: (0xf0, 0xbc, 0x3c),
+    0x29: (0x80, 0xd0, 0x10),
+    0x2a: (0x4c, 0xdc, 0x48),
+    0x2b: (0x58, 0xf8, 0x98),
+    0x2c: (0x00, 0xe8, 0xd8),
+    0x2d: (0x78, 0x78, 0x78),
+    0x30: (0xfc, 0xfc, 0xfc),
+    0x31: (0xa8, 0xe4, 0xfc),
+    0x32: (0xc4, 0xd4, 0xfc),
+    0x33: (0xd4, 0xc8, 0xfc),
+    0x34: (0xfc, 0xc4, 0xfc),
+    0x35: (0xfc, 0xc4, 0xd8),
+    0x36: (0xfc, 0xbc, 0xb0),
+    0x37: (0xfc, 0xd8, 0xa8),
+    0x38: (0xfc, 0xe4, 0xa0),
+    0x39: (0xe0, 0xfc, 0xa0),
+    0x3a: (0xa8, 0xf0, 0xbc),
+    0x3b: (0xb0, 0xfc, 0xcc),
+    0x3c: (0x9c, 0xfc, 0xf0),
+    0x3d: (0xc4, 0xc4, 0xc4),
+}
+
+def get_filenames():
+    # generate names of PNG files without extensions
+    with os.scandir(IMAGE_DIR) as dirIter:
+        yield from (
+            os.path.splitext(e.name)[0] for e in dirIter
+            if not e.is_dir() and os.path.splitext(e.name)[1] == IMAGE_EXT
+        )
+
+def color_diff(rgb1, rgb2):
+    # get difference of two colors (0-768)
+    return 2 * abs(rgb1[0] - rgb2[0]) + 3 * abs(rgb1[1] - rgb2[1]) + abs(rgb1[2] - rgb2[2])
+
+def closest_nes_color(rgb):
+    # get best match (NES color number) for color (red, green, blue)
+
+    minDiff = 9999
+    for nesColor in NES_PALETTE:
+        diff = color_diff(rgb, NES_PALETTE[nesColor])
+        if diff < minDiff:
+            minDiff = diff
+            bestColor = nesColor
+    return bestColor
+
+def get_color_sets(image):
+    # for each attribute block (16*16 px), generate set of color indexes, e.g. {0, 2}
+
+    for ay in range(14):
+        for ax in range(16):
+            indexes = set()
+            for py in range(16):
+                for px in range(16):
+                    indexes.add(image.getpixel((ax*16+px, ay*16+py)))
+            yield indexes
+
+def create_subpalettes(colorSets):
+    # colorSets: set of sets of color indexes in attribute blocks
+    # return: 4 subpalettes (sets) with up to 3 color indexes each
+
+    subpals = [set(), set(), set(), set()]
+
+    for colorSet in colorSets:
+        # get maximum number of common colors with a subpalette in which the new colors fit
+        maxCnt = max(len(subpal & colorSet) for subpal in subpals if len(subpal | colorSet) <= 3)
+        # which subpalette was it (first one if several)
+        bestSubpal = [
+            i for i in range(4)
+            if len(subpals[i] & colorSet) == maxCnt and len(subpals[i] | colorSet) <= 3
+        ][0]
+        # add colors there
+        subpals[bestSubpal].update(colorSet)
+
+    return subpals
+
+def get_attr_data(image, subpals, bgIndex):
+    # generate attribute table data (int 0-3 for each attribute block)
+
+    subpals = [set(sp) | {bgIndex} for sp in subpals]
+    yield from (
+        [i for i in range(4) if subpals[i].issuperset(colorSet)][0]
+        for colorSet in get_color_sets(image)
+    )
+
+def process_flag(handle):
+    handle.seek(0)
+    image = Image.open(handle)
+    if image.width != 256 or image.height != 224 or image.mode != "P":
+        sys.exit("Image must be 256*224 pixels and have a palette.")
+
+    # get used color indexes and best NES equivalents: {pngIndex: nesColor, ...}
+    palette = image.getpalette()  # [R, G, B, ...]
+    palette = dict(
+        (i[1], closest_nes_color(palette[i[1]*3:(i[1]+1)*3]))
+        for i in image.getcolors()
+    )
+    print("Palette (PNG index/NES color):")
+    print(" ", ", ".join(f"{i}=${palette[i]:02x}" for i in sorted(palette)))
+    if len(set(palette.values())) < len(palette):
+        sys.exit("Oops! More than one PNG color maps to same NES color.")
+
+    # get PNG index of NES background color
+    try:
+        bgIndex = [i for i in palette if palette[i] == NES_BG_COLOR][0]
+    except IndexError:
+        bgIndex = -1
+    print(f"PNG index of NES background color (${NES_BG_COLOR:02x}): {bgIndex}")
+
+    # get unique sets of color indexes in attribute blocks, e.g. {{1, 2}, {1, 3}}
+    colorSets = {frozenset(s - {bgIndex}) for s in get_color_sets(image)}
+    print("Unique sets of indexes for attribute blocks (excl. bg color):")
+    print(" ", "; ".join(str(tuple(sorted(s))) for s in colorSets))
+
+    # split sets of color indexes into subpalettes
+    subpals = create_subpalettes(colorSets)
+    subpals = [sorted(sp) for sp in subpals]
+    print("Subpalettes as PNG indexes (excl. bg color):")
+    print(" ", subpals)
+    subpalsNes = [
+        [NES_BG_COLOR] + sorted(palette[c] for c in sp) + (3 - len(sp)) * [NES_BG_COLOR]
+        for sp in subpals
+    ]
+    print("Subpalettes as NES colors (incl. bg color, padded):")
+    print(" ", "; ".join(",".join(f"${c:02x}" for c in sp) for sp in subpalsNes))
+
+    attrData = list(get_attr_data(image, subpals, bgIndex))
+    print("Attribute table data:")
+    print(" ", attrData)
+
+    # TODO: name table data, pattern table data
+
+def main():
+    for file_ in sorted(get_filenames()):
+        print("Flag:", file_)
+        path = os.path.join(IMAGE_DIR, file_) + IMAGE_EXT
+
+        try:
+            with open(path, "rb") as handle:
+                process_flag(handle)
+        except OSError:
+            sys.exit("Error reading file.")
+
+        print()
+
+main()
